96d1371ecb30931ada030cb5a221245a221f5623
ShivenGohil21 <shivengohil21@gmail.com>
2026-06-29 20:42:21 +0530

    Increase JWT expiration limits to prevent rapid 401 session logouts

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,14 +6,14 @@ const { logAction } = require('../utils/auditLogger');
 // Generate JWT token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || '2h'
+    expiresIn: process.env.JWT_EXPIRE || '7d'
   });
 };
 
 // Generate Refresh Token
 const generateRefreshToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: process.env.JWT_REFRESH_EXPIRE || '7d'
+    expiresIn: process.env.JWT_REFRESH_EXPIRE || '30d'
   });
 };
 
